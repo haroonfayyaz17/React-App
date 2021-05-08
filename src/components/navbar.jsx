6e5b3d076817bd04c0d085c89948ca888449d889
Your Name <you@example.com>
2021-05-08 14:37:36 +0500

    Multiple Components in Sync

---
+++ src/components/navbar.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+class NavBar extends Component {
+  render () {
+    return (
+      <nav className='navbar navbar-dark bg-info'>
+        <a className='navbar-brand' href='https://www.facebook.com/'>Facebook</a>
+      </nav>
+    )
+  }
+}
+export default NavBar
