ef64376c88c5a15fa35402e1200432a1fbced593
Your Name <you@example.com>
2021-05-08 14:00:27 +0500

    callback Event and passing counter object

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -1,32 +1,35 @@
 import React, { Component } from 'react'
 
 class Counter extends Component {
- 
   constructor (props) {
     super(props)
     this.state = {
-      count: this.props.value
+      count: this.props.counter.value
     }
     this.handleIncrement = this.handleIncrement.bind(this)
   }
 
-  handleIncrement = product => {
-    console.log(product)
+  handleIncrement = () => {
     this.setState({ count: this.state.count + 1 })
   }
 
   render () {
-    console.log('props', this.props)
     return (
       <div>
         <span className={this.getBadgeClasses()}>{this.formateCount()}</span>
         <button
-          onClick={() => this.handleIncrement({ id: 1 })}
+          onClick={this.handleIncrement}
           className='btn btn-secondary btn-sm'
         >
           Increment
         </button>
-       
+
+        <button
+          onClick={()=>this.props.onDelete(this.props.counter.id)}
+          className='btn btn-danger btn-sm m-2'
+        >
+          Delete
+        </button>
       </div>
     )
   }
@@ -40,7 +43,6 @@ class Counter extends Component {
     const { count } = this.state
     return count === 0 ? <span>Zero</span> : count
   }
-  
 }
 
 export default Counter
